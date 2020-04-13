e993a8ef0b94b69ff2180e6ce9112ae9f9287d3e
jacob kleiman <jacobakleiman@gmail.com>
2020-04-13 12:29:03 -0700

    adds env port

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.post('/video/token', (req, res) => {
   sendTokenResponse(token, res);
 });
 
-app.listen(3001, () =>
+app.listen(process.env.PORT, () =>
   console.log('Express server is running on localhost:3001')
 );
